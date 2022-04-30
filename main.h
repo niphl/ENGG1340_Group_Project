8ea2f1dc758733d2480130ab1ed7718c187371af
niphl <99402880+niphl@users.noreply.github.com>
2022-05-01 03:12:32 +0800

    Add files via upload
    Created more features for flagging
    
    Added quick flagging, convenient cell uncover via flagging
    Added move history recording.
    
    Beginning to add savegame features.

--- main.h
+++ main.h
@@ -3,6 +3,8 @@
 #include <cstdlib>
 #include <string>
 #include <time.h>
+#include <fstream>
+
 using namespace std;
 
 #ifndef main_h
@@ -64,6 +66,9 @@ struct Board{
         srand(seed);
         mineBoard = new int [sizeX * sizeY];
         playerBoard = new char [sizeX * sizeY];
+        moveHistory = new int [32];
+        moveHistory[0] = 0;
+        moveHistory[1] = 32;
         //Initialize the playerBoard;
         for (int i=0; i<sizeX * sizeY;i++){
             playerBoard[i] = 'X';
@@ -94,6 +99,18 @@ struct Board{
         int count = 0;
         for (int i=0; i<sizeX * sizeY;i++){ //Loop accross all cells
             if (adjacent(i%sizeX, i/sizeX, x, y) && mineBoard[i] == 1) {
+                count++;
+            }
+        }
+        return count;
+    }
+    //Function: numAdjacentFlags
+    //Counts the number of flags next to the cell specified by the argument.
+    //Loops through the whole board. Not very scalable, but our game doesn't support massive board sizes anyway.
+    int numAdjacentFlags(int x, int y) {
+        int count = 0;
+        for (int i=0; i<sizeX * sizeY;i++){ //Loop accross all cells
+            if (adjacent(i%sizeX, i/sizeX, x, y) && playerBoard[i] == 'F') {
                 count++;
             }
         }
@@ -169,11 +186,18 @@ struct Board{
     //Yet to be implemented: Flagging calls, fast uncover using flagged cells.
     void prompt_move(){
         int moveX = -1, moveY = -1;
+        bool tempFlagMode = false;
         string userInput;
-        cout << "Please insert a move: " << endl;
+        if (flagMode == false) {
+            cout << "Please uncover a cell: " << endl;
+        }
+        else {
+            cout << "Please flag a cell: " << endl;
+        }
         while ((moveX == -1 || moveY == -1)) {
             cin >> userInput;
             //Handle different cases now.
+            //Toggling Flag Mode:
             if (userInput == "F" || userInput == "f") {
                 if (mineBoard == NULL) {
                     cout << "Make a move first before using flag mode!";
@@ -185,6 +209,24 @@ struct Board{
                 else {
                     cout << "Flag mode turned off!" << endl;
                     flagMode = false;
+                }
+            }
+            //Short form flag move. Probably more complicated than it should be.
+            else if ((userInput[0] == 'F' || userInput[0] == 'f') && (((toupper(userInput[1])-'A')<26) && ((toupper(userInput[1])-'A')>-1))) {
+                //create a temporary string and copy the cell address into it.
+                char moveCopy[4];
+                for (int i=0; i < 3; i++){
+                    moveCopy[i] = userInput[i+1];
+                }
+                moveCopy[3] = '\0';
+                moveX = strToX(moveCopy, sizeX);
+                moveY = strToY(moveCopy, sizeY);
+                if (moveX == -1 || moveY == -1) {
+                    cout << "Invalid move! Please try again" << endl;
+                }
+                else {
+                    flagMode = true;
+                    tempFlagMode = true;
                 }
             }
             else {
@@ -199,33 +241,58 @@ struct Board{
         if (mineBoard == NULL) {
             initialize(moveX, moveY);
             uncover(moveX, moveY);
-            cout << "initializing.." << endl;
+            recordMove (moveX, moveY, false);
             print_board();
         }
         //Cell already uncovered
         else if (playerBoard[moveY*sizeX + moveX] == 'U') {
+            //For uncovering cells where the number of adjacent flags is correct.
+            if (numAdjacentMines(moveX, moveY) == numAdjacentFlags(moveX, moveY)) {
+                int cellsUncovered = 0;
+                for (int i=0; i<sizeX * sizeY;i++){ //Loop accross all cells
+                    if (adjacent(i%sizeX, i/sizeX, moveX, moveY) && playerBoard[i] == 'X') {
+                        uncover(i%sizeX, i/sizeX);
+                        recordMove (i%sizeX, i/sizeX, false);
+                        cellsUncovered++;
+                    }
+                }
+                //If there's nothing to uncover, we give error message. Else, we print board.
+                if (cellsUncovered == 0) {
+                    cout << "Cell already uncovered! Please try again." << endl;
+                }
+                else {
+                    print_board();
+                }
+            }
+            else {
             cout << "Cell already uncovered! Please try again." << endl;
+            }
         }
         //Regular move
         else if (flagMode == false) {
-            //Need code: Store move in data
             if (playerBoard[moveY*sizeX + moveX] == 'F') {
                 cout << "Cell is flagged, so can't be uncovered! Please Unflag first." << endl;
             }
             else {
                 uncover(moveX, moveY);
                 print_board();
+                recordMove (moveX, moveY, false);
             }
         }
         //Flag Mode Handling
         else {
             flagMove(moveX, moveY);
             print_board();
-            //Need code: Store Move in Data
+            recordMove (moveX, moveY, true);
+        }
+        //For short-form flag, we turn off flag mode again
+        if (tempFlagMode == true) {
+            flagMode = false;
         }
         prompt_move(); //If game not ended, we prompt.
     }
-    //How our program handles when the player flags a cell.
+    //Function: flagMove
+    //Unflags a flagged cell, or flags an unflagged cell.
     void flagMove(int x, int y){
         //If it's flagged, unflag it.
         if (playerBoard[y*sizeX + x] == 'F') {
@@ -235,6 +302,49 @@ struct Board{
         else if (playerBoard[y*sizeX + x] == 'X') {
             playerBoard[y*sizeX + x] = 'F';
         }
+    }
+    //Function: recordMove
+    //Records the player's move into the array
+    void recordMove (int x, int y, bool flag) {
+        //0th cell of moveHistory states the cell num we will input the move into
+        //1st cell of moveHistory states the size of the array
+        //If they are the same, we must expand the array (Dynamic Memory)
+        if (moveHistory[0] == moveHistory[1]) {
+            int * temp = new int [moveHistory[1] + 32];
+            for (int i = 0; i < moveHistory[1]; ++i){
+                temp[i] = moveHistory[i];
+            }
+            delete [] moveHistory;
+            moveHistory = temp;
+            moveHistory[1] = moveHistory[1] + 32;
+        }
+        //We code it as follows: Move = 100 * x coordinate + y coordinate
+        //If it is not a flag, we add 10,000. If it is a flag, we add 20,000.
+        moveHistory[moveHistory[0]] = 10000 + 100*x + y;
+        if (flag == true) {
+            moveHistory[moveHistory[0]] += 10000;
+        }
+        moveHistory[0] += 1;
+        saveGame(); //Autosave
+    }
+    //Function: saveGame
+    //Saves the board state into a file.
+    void saveGame(){
+        ofstream fout;
+        fout.open("savegame.txt");
+        if (fout.fail()){
+            cout << "Error writing to file." << endl;
+            return;
+        }
+        fout << difficulty << endl;
+        fout << sizeX << endl;
+        fout << sizeY << endl;
+        fout << numMines << endl;
+        fout << seed << endl;
+        for (int i=0;i<moveHistory[0];i++){
+            fout << moveHistory[i] << endl;
+        }
+        fout.close();
     }
 };
 
